ddc824411eb44dbe03d93f8707b5ced9a5ed0d09
Kaushal Dhakal <kaushaldhakal90@gmail.com>
2021-06-23 20:09:29 +0545

    data fetching

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const reduxLogger = require("redux-logger");
+
+const logger = reduxLogger.createLogger();
+const applyMiddleware = redux.applyMiddleware;
+
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
-
 // ACTION
 function buyCake() {
   return {
     type: BUY_CAKE,
-    info: "First redux action",
   };
 }
 
@@ -17,24 +21,27 @@ function buyIceCream() {
   };
 }
 
-//INITIAL STATE
-const initialState = {
+// INITIAL STATE
+// const initialState = {
+//   noOfCakes: 10,
+//   noOfICeCreams: 20,
+// };
+
+const cakeState = {
   noOfCakes: 10,
+};
+
+const iceCreamState = {
   noOfICeCreams: 20,
 };
 
 //REDUCER FUNCTION
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = cakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         noOfCakes: state.noOfCakes - 1,
-      };
-    case BUY_ICECREAM:
-      return {
-        ...state,
-        noOfICeCreams: state.noOfICeCreams - 1,
       };
 
     default:
@@ -42,15 +49,32 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+const iceCreamReducer = (state = iceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        noOfICeCreams: state.noOfICeCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+//combining reducer
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
 //create redux store
-const store = createStore(reducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log("INITIAL STATE::", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
